a4063de6adeea4486990c70701c22bc568384fd2
essi <esmail.mirhoseini@gmail.com>
2019-03-02 19:19:29 +0330

    validation

--- app/Http/Controllers/ConcertOrderController.php
+++ app/Http/Controllers/ConcertOrderController.php
@@ -8,16 +8,22 @@ use App\Concert;
 class ConcertOrderController extends Controller
 {
 	private $paymentGeteway;
+	
 	public function __construct(PaymentGeteway $paymentGeteway){
 		$this->paymentGeteway = $paymentGeteway;
 	}
     public function store($concertId,Request $request){
+    	$this->validate(request(),[
+    		"email" => "required",
+            "ticket_quantity" => "integer|required|min:1",
+            "_token" => "required"
+    	]);
     	$concert = Concert::find($concertId);
     	
     	
     	$this->paymentGeteway->charge($request->ticket_quantity * $concert->ticket_price,$request->_token);
     	$order = $concert->orderTickets($request->email,$request->ticket_quantity);
-    	
+
     	return response()->json([],201);
     }
 }
--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -11,26 +11,107 @@ use App\Billing\PaymentGeteway;
 class PurchaseTicketsTest extends TestCase
 {
     use DatabaseMigrations;
+    // private $paymentGeteway;
+    protected function setUp(){
+        parent::setUp();
+        $this->paymentGeteway = new FakePaymentGeteway;
+        $this->app->instance(PaymentGeteway::class,$this->paymentGeteway);
+        
+    }
+
+    public function orderTickets($concert,$params){
+        return $this->json("post","concert/".$concert->id."/orders",$params);
+    }
 
     /**
      @test
      */
     public function customer_can_purchase_concert_tickets()
     {
-        $paymentGeteway = new FakePaymentGeteway();
-        $this->app->instance(PaymentGeteway::class,$paymentGeteway);
+      
 
         $concert = factory(Concert::class)->state("publiched")->create(["ticket_price" => 1500]);
-        $response = $this->json("post","concert/".$concert->id."/orders",[
+        $response = $this->orderTickets($concert,[
             "email" => "a@b.com",
             "ticket_quantity" => 3,
-            "_token" => $paymentGeteway->getValidTestTocken()
+            "_token" => $this->paymentGeteway->getValidTestTocken()
         ]);
         $response->assertStatus(201);
 
-        $this->assertEquals(4500, $paymentGeteway->totalCharges());
+        $this->assertEquals(4500, $this->paymentGeteway->totalCharges());
         $order = $concert->orders()->where("email","a@b.com")->first();
         $this->assertNotNull($order);
         $this->assertEquals(3,$order->tickets()->count());
+    }
+    /**
+    @test
+    */
+    public function email_is_required_to_purchase(){
+
+
+        $concert = factory(Concert::class)->state("publiched")->create();
+        $response = $this->orderTickets($concert,[
+            "ticket_quantity" => 3,
+            "_token" => $this->paymentGeteway->getValidTestTocken()
+        ]);
+        // dd($response->responce());
+        $response->assertStatus(422);
+
+        // $response->assertArrayHasKey('email',$response->decodeResponceJson());
+
+    }
+    /**
+    @test
+    */
+    public function ticket_quantity_is_required_to_purchase(){
+
+
+        $concert = factory(Concert::class)->state("publiched")->create();
+        $response = $this->orderTickets($concert,[
+            "email" => "a@b.com",
+            "_token" => $this->paymentGeteway->getValidTestTocken()
+        ]);
+        // dd($response->responce());
+        $response->assertStatus(422);
+
+        // $response->assertArrayHasKey('email',$response->decodeResponceJson());
+
+    }
+    /**
+    @test
+    */
+    public function ticket_quantity_must_min_1_token(){
+
+
+        $concert = factory(Concert::class)->state("publiched")->create();
+        $response = $this->orderTickets($concert,[
+            "email" => "a@b.com",
+            "ticket_quantity" => 0,
+            "_token" => $this->paymentGeteway->getValidTestTocken()
+        ]);
+        // dd($response->responce());
+        $response->assertStatus(422);
+
+        // $response->assertArrayHasKey('email',$response->decodeResponceJson());
+
+    }
+    /**
+    @test
+    */
+    public function token_is_required_to_purchase(){
+
+
+        $concert = factory(Concert::class)->state("publiched")->create();
+        $response = $this->orderTickets($concert,[
+            "email" => "a@b.com",
+            "ticket_quantity" => 3
+        ]);
+        // dd($response->responce());
+
+        
+        $response->assertStatus(422);
+
+        // $response->assertArrayHasKey('email',$response->decodeResponceJson());
+
     }
 }
